ce4dea6ff05500bfce3f016ee67372f3c9339773
cgamboasanabria <cesar.gamboasanabria@ucr.ac.cr>
2020-06-28 22:39:19 -0600

    update

--- R/optimal_stage.R
+++ R/optimal_stage.R
@@ -27,11 +27,12 @@ optimal_stage <- function(data, limit=80){
       ggplot(., aes(x=columns, y=error))+
       geom_line(color="orange", size=0.8)+
       theme_bw()+
-      scale_x_continuous(expand = c(0,0))+
+      scale_x_continuous(expand = c(0,0), limits=c(min(data$columns), max(data$columns)*1.10))+
       scale_y_continuous(expand = c(0,0))+
-      geom_vline(xintercept = best_columns$columns, color="red", show.legend = "hola")+
+      geom_vline(xintercept = best_columns$columns, color="red")+
       geom_text(mapping = aes(label = paste("columns=", best_columns$columns),
-                              x = best_columns$columns, y=median(error)), angle = 60, hjust = 0)
+                              x = best_columns$columns, y=median(error)), angle = 60, hjust = 0)+
+      labs(y="Average relative error", x="Number of columns", title="Average relative error by number of columns")
 
   }else({
     best_columns <- data.frame(columns=unique(data$columns))
@@ -59,11 +60,12 @@ optimal_stage <- function(data, limit=80){
       ggplot(., aes(x=rows, y=error))+
       geom_line(color="orange", size=0.8)+
       theme_bw()+
-      scale_x_continuous(expand = c(0,0))+
+      scale_x_continuous(expand = c(0,0), limits=c(min(data$rows), max(data$rows)*1.10))+
       scale_y_continuous(expand = c(0,0))+
       geom_vline(xintercept = best_rows$rows, color="red")+
       geom_text(mapping = aes(label = paste("rows=", best_rows$rows),
-                              x = best_rows$rows, y=median(error)), angle = 60, hjust = 0)
+                              x = best_rows$rows, y=median(error)), angle = 60, hjust = 0)+
+      labs(y="Average relative error", x="Number of rows", title="Average relative error by number of rows")
 
   }else({
     best_rows <- data.frame(rows=unique(data$rows))
@@ -91,9 +93,13 @@ optimal_stage <- function(data, limit=80){
     scale_y_continuous(expand = c(0,0))+
     geom_vline(xintercept = best_k$k, color="red")+
     geom_text(mapping = aes(label = paste("k=", best_k$k),
-                            x = best_k$k, y=median(error)), angle = 60, hjust = 0)
+                            x = best_k$k, y=median(error)), angle = 60, hjust = 0)+
+    labs(y="Average relative error", x="k",
+         title=paste("Average relative error with", best_columns$columns, "columns and ", best_rows$rows, "rows by k", sep = " "))
 
   data <- arrange(data, error)
-
-  list(data=data, columns_plot=columns_plot, rows_plot=rows_plot, k_plot=k_plot, optimal=data.frame(best_k, best_columns, best_rows))
+  error <- filter(data, columns==best_columns$columns & rows==best_rows$rows & k==best_k$k) %>%
+    dplyr::select(error)
+  list(data=data, columns_plot=columns_plot, rows_plot=rows_plot, k_plot=k_plot,
+       optimal=data.frame(best_k, best_columns, best_rows, error))
 }
