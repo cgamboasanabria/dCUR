2ae7af5f731ebccee6f9e0939bd401170cbde627
cgamboasanabria <cesar.gamboasanabria@ucr.ac.cr>
2020-06-28 21:22:58 -0600

    update optimal_stage

--- R/optimal_stage.R
+++ R/optimal_stage.R
@@ -7,31 +7,70 @@ optimal_stage <- function(data, limit=80){
     mutate(stage=row.names(.)) %>%
     arrange(error, desc(k), desc(columns), desc(rows))
 
-  best_columns <- data %>%
-    group_by(columns) %>%
-    summarise(error=mean(error)) %>%
-    mutate(rate=lag(error),
-           rate2=rate-error,
-           rate2=rate2/rate) %>%
-    filter(!is.na(rate2)) %>%
-    mutate(rate2=rate2/sum(rate2)*100,
-           rate2=cumsum(rate2)) %>%
-    filter(rate2>=limit)
+  if(unique(data$columns)>=2){
+    best_columns <- data %>%
+      group_by(columns) %>%
+      summarise(error=mean(error)) %>%
+      mutate(rate=lag(error),
+             rate2=rate-error,
+             rate2=rate2/rate) %>%
+      filter(!is.na(rate2)) %>%
+      mutate(rate2=rate2/sum(rate2)*100,
+             rate2=cumsum(rate2)) %>%
+      filter(rate2>=limit)
 
-  best_columns <- best_columns[1,1]
+    best_columns <- best_columns[1,1]
 
-  best_rows <- data %>%
-    group_by(rows) %>%
-    summarise(error=mean(error)) %>%
-    mutate(rate=lag(error),
-           rate2=rate-error,
-           rate2=rate2/rate) %>%
-    filter(!is.na(rate2)) %>%
-    mutate(rate2=rate2/sum(rate2)*100,
-           rate2=cumsum(rate2)) %>%
-    filter(rate2>=limit)
+    columns_plot <- data %>%
+      group_by(columns) %>%
+      summarise(error=mean(error)) %>%
+      ggplot(., aes(x=columns, y=error))+
+      geom_line(color="orange", size=0.8)+
+      theme_bw()+
+      scale_x_continuous(expand = c(0,0))+
+      scale_y_continuous(expand = c(0,0))+
+      geom_vline(xintercept = best_columns$columns, color="red", show.legend = "hola")+
+      geom_text(mapping = aes(label = paste("columns=", best_columns$columns),
+                              x = best_columns$columns, y=median(error)), angle = 60, hjust = 0)
 
-  best_rows <- best_rows[1,1]
+  }else({
+    best_columns <- data.frame(columns=unique(data$columns))
+    columns_plot <- NULL
+  })
+
+
+  if(unique(data$rows)>=2){
+    best_rows <- data %>%
+      group_by(rows) %>%
+      summarise(error=mean(error)) %>%
+      mutate(rate=lag(error),
+             rate2=rate-error,
+             rate2=rate2/rate) %>%
+      filter(!is.na(rate2)) %>%
+      mutate(rate2=rate2/sum(rate2)*100,
+             rate2=cumsum(rate2)) %>%
+      filter(rate2>=limit)
+
+    best_rows <- best_rows[1,1]
+
+    rows_plot <- data %>%
+      group_by(rows) %>%
+      summarise(error=mean(error)) %>%
+      ggplot(., aes(x=rows, y=error))+
+      geom_line(color="orange", size=0.8)+
+      theme_bw()+
+      scale_x_continuous(expand = c(0,0))+
+      scale_y_continuous(expand = c(0,0))+
+      geom_vline(xintercept = best_rows$rows, color="red")+
+      geom_text(mapping = aes(label = paste("rows=", best_rows$rows),
+                              x = best_rows$rows, y=median(error)), angle = 60, hjust = 0)
+
+  }else({
+    best_rows <- data.frame(rows=unique(data$rows))
+
+    rows_plot <- NULL
+  })
+
 
   best_k <- data %>%
     filter(columns==best_columns$columns & rows==best_rows$rows) %>%
@@ -40,30 +79,6 @@ optimal_stage <- function(data, limit=80){
     arrange(error)
 
   best_k <- best_k[1,1]
-
-  columns_plot <- data %>%
-    group_by(columns) %>%
-    summarise(error=mean(error)) %>%
-    ggplot(., aes(x=columns, y=error))+
-    geom_line(color="orange", size=0.8)+
-    theme_bw()+
-    scale_x_continuous(expand = c(0,0))+
-    scale_y_continuous(expand = c(0,0))+
-    geom_vline(xintercept = best_columns$columns, color="red", show.legend = "hola")+
-    geom_text(mapping = aes(label = paste("columns=", best_columns$columns),
-                            x = best_columns$columns, y=median(error)), angle = 60, hjust = 0)
-
-  rows_plot <- data %>%
-    group_by(rows) %>%
-    summarise(error=mean(error)) %>%
-    ggplot(., aes(x=rows, y=error))+
-    geom_line(color="orange", size=0.8)+
-    theme_bw()+
-    scale_x_continuous(expand = c(0,0))+
-    scale_y_continuous(expand = c(0,0))+
-    geom_vline(xintercept = best_rows$rows, color="red")+
-    geom_text(mapping = aes(label = paste("rows=", best_rows$rows),
-                            x = best_rows$rows, y=median(error)), angle = 60, hjust = 0)
 
   k_plot <- data %>%
     filter(columns==best_columns$columns & rows==best_rows$rows) %>%
