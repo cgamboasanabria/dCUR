57cd5c8144065b9ee9777377aa21065451ca0032
cgamboasanabria <cesar.gamboasanabria@ucr.ac.cr>
2020-07-01 20:02:00 -0600

    Update globals.R

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,4 @@
-utils::globalVariables(c(".", "desc", "components"))
+utils::globalVariables(c(".", "desc", "components","type","BIC","..density..","x","y","Leverage",
+                         "Cumulative density function", "ppoints","theorical" "quantiles",
+                         "sample quantiles", "k", "columns", "rows", "lag", "rate", "rate2", "median",
+                         "leverage_columns","density","reorder","var_names"))
